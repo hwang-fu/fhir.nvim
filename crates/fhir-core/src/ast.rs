86bebfcbab82d47b004f1ca7ad263ba068aa9513
hwangfucius <junzhe.hwangfu@gmail.com>
2026-06-09 22:39:24 +0200

    feat(core): fhirpath ast

---
+++ crates/fhir-core/src/ast.rs
@@ -0,0 +1,66 @@
+use rust_decimal::Decimal;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum Expr {
+    Literal(Literal),
+    This,
+    Identifier(String),
+    Member {
+        base: Box<Expr>,
+        name: String,
+    },
+    Index {
+        base: Box<Expr>,
+        index: Box<Expr>,
+    },
+    Call {
+        base: Option<Box<Expr>>,
+        name: String,
+        args: Vec<Expr>,
+    },
+    Binary {
+        op: BinOp,
+        lhs: Box<Expr>,
+        rhs: Box<Expr>,
+    },
+    TypeTest {
+        op: TypeOp,
+        expr: Box<Expr>,
+        type_name: String,
+    },
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, PartialEq)]
+pub enum Literal {
+    Boolean(bool),
+    Integer(i64),
+    Decimal(Decimal),
+    Str(String),
+    Date(String),
+    DateTime(String),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BinOp {
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+    And,
+    Or,
+    In,
+    Union,
+    Concat,
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum TypeOp {
+    Is,
+    As,
+}
